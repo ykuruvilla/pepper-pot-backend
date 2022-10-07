ba86cef515f5eac8bd8a254c920eaf9f773910a4
ykuruvilla <yasha.kuruvilla@gmail.com>
2022-10-07 15:21:35 +0100

    initial data for testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const { res, req } = require("express");
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
